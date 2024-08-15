79f5ff9cad40caa6f9734f2b4c76f97e3b727c69
User <thisisoly1@gmail.com>
2024-08-15 12:50:30 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/rest_api_assignmennt_oly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rest_api_assignmennt_oly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Models/detail_page_model.dart
@@ -0,0 +1,10 @@
+/*import 'package:rest_api_assignment_ostad/Models/photo_model.dart';*/
+
+import 'package:rest_api_assignmennt_oly/Models/photo_model.dart';
+
+class DetailPageModel{
+  PhotoModel photoModel;
+  int index;
+
+  DetailPageModel(this.photoModel, this.index);
+}
